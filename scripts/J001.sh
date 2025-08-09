7fb75a3c59fd1fc50e529fc1f52c9e2f18d6fdc0
hibana2077 <hibana2077@gmail.com>
2025-08-09 22:04:47 +0800

    fix: increase walltime to 2.5 hours and update pkb-views to 20 in training script

--- scripts/J001.sh
+++ scripts/J001.sh
@@ -4,7 +4,7 @@
 #PBS -l ngpus=1            
 #PBS -l ncpus=16            
 #PBS -l mem=32GB           
-#PBS -l walltime=02:00:00  
+#PBS -l walltime=02:30:00  
 #PBS -l wd                  
 #PBS -l storage=scratch/rp06
 
@@ -15,4 +15,4 @@ source /scratch/rp06/sl5952/PKB/.venv/bin/activate
 
 cd ..
 # Run training with PKB models
-python3 train.py --dataset cotton80 --model resnet50 --pretrained --augmentation pkb --pkb-n 4 --pkb-a-frac 0.25 --pkb-sigma 2.0 --pkb-views 10 --pkb-placement random --color-jitter --hflip --rotate --save-best >> ct80_r50_pkbaug.log
+python3 train.py --dataset cotton80 --model resnet50 --pretrained --augmentation pkb --pkb-n 4 --pkb-a-frac 0.25 --pkb-sigma 2.0 --pkb-views 20 --pkb-placement random --color-jitter --hflip --rotate --save-best >> ct80_r50_pkbaug.log
