34837ba7b1dd94bc791e23784e86e3f6e2745e8f
hibana2077 <hibana2077@gmail.com>
2025-08-11 19:38:12 +0800

    fix: update EXA002 and train.py scripts to remove mixed precision flags and optimize training process

--- scripts/EXA002.sh
+++ scripts/EXA002.sh
@@ -15,4 +15,4 @@ source /scratch/rp06/sl5952/PKB/.venv/bin/activate
 
 cd ..
 # Run training with PKB models
-python3 train.py --dataset soybean --model tiny_vit_21m_384.dist_in22k_ft_in1k --pretrained --augmentation pkb --pkb-n 6 --pkb-a-frac 0.25 --pkb-sigma 2.0 --pkb-views 16 --amp --amp-dtype bf16 --pkb-placement random --color-jitter --hflip --rotate --save-best >> EXA001.log
+python3 train.py --dataset soybean --model tiny_vit_21m_384.dist_in22k_ft_in1k --pretrained --augmentation pkb --pkb-n 6 --pkb-a-frac 0.25 --pkb-sigma 2.0 --pkb-views 16 --pkb-placement random --color-jitter --hflip --rotate --save-best >> EXA001.log
