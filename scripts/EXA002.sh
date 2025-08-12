9482ba32439d84be9d676415a14e1608eae2e71d
hibana2077 <hibana2077@gmail.com>
2025-08-12 10:13:32 +0800

    fix: update EXA002.sh script to adjust GPU and CPU resource allocation for training

--- scripts/EXA002.sh
+++ scripts/EXA002.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -P rp06
 #PBS -q gpuvolta
-#PBS -l ngpus=4           
-#PBS -l ncpus=48           
+#PBS -l ngpus=1           
+#PBS -l ncpus=12           
 #PBS -l mem=40GB           
 #PBS -l walltime=01:05:00  
 #PBS -l wd                  
@@ -15,4 +15,4 @@ source /scratch/rp06/sl5952/PKB/.venv/bin/activate
 
 cd ..
 # Run training with PKB models
-python3 train.py --dataset soybean --model tiny_vit_21m_384.dist_in22k_ft_in1k --pretrained --augmentation pkb --pkb-n 6 --pkb-a-frac 0.25 --pkb-sigma 2.0 --pkb-views 16 --pkb-placement random --color-jitter --hflip --rotate --save-best --multi-gpu >> EXA002.log
+python3 train.py --dataset soybean --model tiny_vit_21m_384.dist_in22k_ft_in1k --pretrained --augmentation pkb --pkb-n 6 --pkb-a-frac 0.25 --pkb-sigma 2.0 --pkb-views 16 --pkb-placement random --color-jitter --hflip --rotate --save-best --batch-size 8 >> EXA002.log
