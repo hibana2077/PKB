ac680169f3aaea1fb0aedb3c77ced68b2d3be063
hibana2077 <hibana2077@gmail.com>
2025-08-12 15:22:49 +0800

    feat: add EXA003.sh script for training Tiny ViT on SoyAgeing-R1 dataset

---
+++ scripts/EXA003.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q dgxa100
+#PBS -l ngpus=1
+#PBS -l ncpus=16
+#PBS -l mem=32GB
+#PBS -l walltime=04:00:00
+#PBS -l wd
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+# module load python3/3.10.4
+
+source /scratch/rp06/sl5952/PKB/.venv/bin/activate
+
+cd ..
+# Run training with PKB models
+python3 train.py --dataset soy_ageing_r1 --model tiny_vit_21m_384.dist_in22k_ft_in1k --pretrained --augmentation pkb --pkb-n 6 --pkb-a-frac 0.25 --pkb-sigma 2.0 --pkb-views 8 --pkb-placement random --color-jitter --hflip --rotate --save-best >> EXA003.log
