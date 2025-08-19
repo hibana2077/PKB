27ae6b2f48523bff9d64e990aca67a23f3cb80f1
hibana2077 <hibana2077@gmail.com>
2025-08-19 17:54:48 +0800

    fix: remove unnecessary augmentation flag from E003 training script fix: update validation accuracy for TVC008 in training scripts table

--- scripts/E003.sh
+++ scripts/E003.sh
@@ -14,4 +14,4 @@ source /scratch/rp06/sl5952/PKB/.venv/bin/activate
 
 cd ..
 # Run training with PKB models
-python3 train.py --dataset soygene --model efficientnet_b0.ra4_e3600_r224_in1k --pretrained --augmentation base --color-jitter --hflip --rotate --train-crop 224 --save-best >> E003.log
+python3 train.py --dataset soygene --model efficientnet_b0.ra4_e3600_r224_in1k --pretrained --color-jitter --hflip --rotate --train-crop 224 --save-best >> E003.log
