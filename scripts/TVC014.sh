09ac1bd10980f331846f71ef6af2b217c3882145
hibana2077 <hibana2077@gmail.com>
2025-08-20 20:45:20 +0800

    feat: add training scripts for TVC013 and TVC014 models on SoyAgeing-R3 dataset

---
+++ scripts/TVC014.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q dgxa100
+#PBS -l ngpus=1
+#PBS -l ncpus=16
+#PBS -l mem=24GB
+#PBS -l walltime=05:00:00
+#PBS -l wd
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+
+source /scratch/rp06/sl5952/PKB/.venv/bin/activate
+
+cd ..
+# Run training for TVC014 (SoyAgeing-R3) - PKB params from Table.md
+python3 train.py --dataset soy_ageing_r3 --model tiny_vit_21m_384.dist_in22k_ft_in1k --pretrained --augmentation pkb --pkb-n 8 --pkb-a-frac 0.20 --pkb-sigma 2.0 --pkb-views 10 --pkb-placement random --save-best >> TVC014.log
