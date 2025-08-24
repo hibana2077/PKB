74546e1423b9178fb25b5971311331690f27b6d2
hibana2077 <hibana2077@gmail.com>
2025-08-24 11:35:11 +0800

    fix: update walltime in J009 script to 02:00:00 and adjust PKB parameters in Table.md

--- scripts/J009.sh
+++ scripts/J009.sh
@@ -4,7 +4,7 @@
 #PBS -l ngpus=1            
 #PBS -l ncpus=12            
 #PBS -l mem=20GB           
-#PBS -l walltime=00:55:00  
+#PBS -l walltime=02:00:00  
 #PBS -l wd                  
 #PBS -l storage=scratch/rp06
 
@@ -15,4 +15,4 @@ source /scratch/rp06/sl5952/PKB/.venv/bin/activate
 
 cd ..
 # Run training with PKB models
-python3 train.py --dataset cotton80 --model resnet50 --pretrained --augmentation pkb --pkb-n 16 --pkb-a-frac 0.25 --pkb-sigma 4.0 --pkb-views 4 --pkb-placement random --color-jitter --hflip --rotate --save-best >> J009.log
+python3 train.py --dataset cotton80 --model resnet50 --pretrained --augmentation pkb --pkb-n 8 --pkb-a-frac 0.25 --pkb-sigma 2.0 --pkb-views 8 --pkb-placement random --hflip --rotate --save-best >> J009.log
