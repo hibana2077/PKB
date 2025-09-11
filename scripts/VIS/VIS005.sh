601eb7d7042418a56fa0a17ca596669926873c07
hibana2077 <hibana2077@gmail.com>
2025-09-11 21:15:18 +0800

    feat: Add VIS005 script for visualization with CUB-200-2011 dataset using Tiny ViT model

---
+++ scripts/VIS/VIS005.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q gpuvolta
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -l mem=8GB
+#PBS -l walltime=00:05:00
+#PBS -l wd
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+# module load python3/3.10.4
+
+source /scratch/rp06/sl5952/PKB/.venv/bin/activate
+
+cd ../..
+python3 vis.py --checkpoint ./outputs/best_tiny_vit_21m_384.dist_in22k_ft_in1k_cub_200_2011.pth --dataset cub_200_2011 --model tiny_vit_21m_384.dist_in22k_ft_in1k --split test --do-vit-attn --vit-attn-samples 8 --device cuda --out-dir ./outputs/vis_attn_cub_200_2011_tiny_vit_test
