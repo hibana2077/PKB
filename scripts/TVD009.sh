ac8e363fc58592de1a595c707aa9c51f26cb2db8
hibana2077 <hibana2077@gmail.com>
2025-09-14 14:16:58 +0800

    fix: Update training scripts for tiny ViT models to correct sigma and placement parameters

--- scripts/TVD009.sh
+++ scripts/TVD009.sh
@@ -15,4 +15,4 @@ source /scratch/rp06/sl5952/PKB/.venv/bin/activate
 
 cd ..
 # Run training with PKB models
-python3 train.py --dataset cub_200_2011 --model tiny_vit_21m_384.dist_in22k_ft_in1k --pretrained --augmentation pkb --pkb-n 4 --pkb-a-frac 0.25 --pkb-sigma 4.0 --pkb-views 2 --pkb-placement dispersed --hflip --rotate --save-best >> TVD009.log
+python3 train.py --dataset cub_200_2011 --model tiny_vit_21m_384.dist_in22k_ft_in1k --pretrained --augmentation pkb --pkb-n 4 --pkb-a-frac 0.25 --pkb-sigma 5.0 --pkb-views 2 --pkb-placement contiguous --hflip --rotate --save-best >> TVD009.log
