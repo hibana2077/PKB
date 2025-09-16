e72225ad86bcc5d4d1407462b755564489e042a5
hibana2077 <hibana2077@gmail.com>
2025-09-16 15:26:06 +0800

    fix: Add --orig-pic flag to vis.py command in VIS006.sh for original picture output

--- scripts/VIS/VIS006.sh
+++ scripts/VIS/VIS006.sh
@@ -14,4 +14,4 @@ module load cuda/12.6.2
 source /scratch/rp06/sl5952/PKB/.venv/bin/activate
 
 cd ../..
-python3 vis.py --checkpoint ./outputs/best_tiny_vit_21m_384.dist_in22k_ft_in1k_cotton80.pth --dataset cotton80 --model tiny_vit_21m_384.dist_in22k_ft_in1k --split test --do-vit-attn --vit-attn-samples 8 --device cuda --out-dir ./outputs/vis_attn_cotton80_tiny_vit_test
+python3 vis.py --checkpoint ./outputs/best_tiny_vit_21m_384.dist_in22k_ft_in1k_cotton80.pth --dataset cotton80 --model tiny_vit_21m_384.dist_in22k_ft_in1k --split test --do-vit-attn --vit-attn-samples 8 --device cuda --orig-pic --out-dir ./outputs/vis_attn_cotton80_tiny_vit_test
