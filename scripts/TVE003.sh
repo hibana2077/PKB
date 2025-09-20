4079ffd42e37341563b41c010ad4de506294881b
hibana2077 <hibana2077@gmail.com>
2025-09-21 00:09:28 +0800

    fix: add output directory to training command in TVE003 script

--- scripts/TVE003.sh
+++ scripts/TVE003.sh
@@ -15,4 +15,4 @@ source /scratch/rp06/sl5952/PKB/.venv/bin/activate
 
 cd ..
 # Run training with PKB models (SoyGene - TVE003)
-python3 train.py --dataset soygene --model tiny_vit_21m_384.dist_in22k_ft_in1k --pretrained --augmentation pkb --pkb-n 8 --pkb-a-frac 0.20 --pkb-sigma 2.0 --pkb-views 4 --pkb-placement dispersed --hflip --rotate --save-best >> TVE003.log
+python3 train.py --dataset soygene --model tiny_vit_21m_384.dist_in22k_ft_in1k --pretrained --augmentation pkb --pkb-n 8 --pkb-a-frac 0.20 --pkb-sigma 2.0 --pkb-views 4 --pkb-placement dispersed --hflip --rotate --save-best --output ./outputs/tve003 >> TVE003.log
