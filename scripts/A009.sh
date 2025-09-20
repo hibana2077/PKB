a96d0d5b18d6a6befa4e6c9de29c60f471eec99b
hibana2077 <hibana2077@gmail.com>
2025-09-21 00:37:47 +0800

    feat: add output directory and stop-at-top1 option to A009 training script

--- scripts/A009.sh
+++ scripts/A009.sh
@@ -15,4 +15,4 @@ source /scratch/rp06/sl5952/PKB/.venv/bin/activate
 
 cd ..
 # Run training with PKB models
-python3 train.py --dataset cub_200_2011 --model tiny_vit_21m_384.dist_in22k_ft_in1k --pretrained --color-jitter --hflip --rotate --save-best >> A009.log
+python3 train.py --dataset cub_200_2011 --model tiny_vit_21m_384.dist_in22k_ft_in1k --pretrained --color-jitter --hflip --rotate --save-best --output ./outputs/a009 --stop-at-top1 0.80 >> A009.log
