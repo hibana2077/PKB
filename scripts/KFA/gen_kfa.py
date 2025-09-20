92b9bf290e5b0f4ff61c782cf4022bb52bd7c030
hibana2077 <hibana2077@gmail.com>
2025-09-20 12:57:28 +0800

    fix: Change project

--- scripts/KFA/gen_kfa.py
+++ scripts/KFA/gen_kfa.py
@@ -22,19 +22,19 @@ def script_content(code: str, placement: str, n: int, a_frac: float, sigma: floa
     # Log file matches code name in repo root
     log_name = f"{code}.log"
     return f"""#!/bin/bash
-#PBS -P rp06
+#PBS -P cp23
 #PBS -q gpuvolta
 #PBS -l ngpus=1            
 #PBS -l ncpus=12            
 #PBS -l mem=20GB           
 #PBS -l walltime=24:30:00  
 #PBS -l wd                  
-#PBS -l storage=scratch/rp06
+#PBS -l storage=scratch/cp23
 
 module load cuda/12.6.2
 # module load python3/3.10.4
 
-source /scratch/rp06/sl5952/PKB/.venv/bin/activate
+source /scratch/cp23/sl5952/PKB/.venv/bin/activate
 
 cd ../..
 
--- scripts/KFA/kfa044.sh
+++ scripts/KFA/kfa044.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#PBS -P rp06
+#PBS -P cp23
 #PBS -q gpuvolta
 #PBS -l ngpus=1            
 #PBS -l ncpus=12            
 #PBS -l mem=20GB           
 #PBS -l walltime=24:30:00  
 #PBS -l wd                  
-#PBS -l storage=scratch/rp06
+#PBS -l storage=scratch/cp23
 
 module load cuda/12.6.2
 # module load python3/3.10.4
 
-source /scratch/rp06/sl5952/PKB/.venv/bin/activate
+source /scratch/cp23/sl5952/PKB/.venv/bin/activate
 
 cd ../..
 
